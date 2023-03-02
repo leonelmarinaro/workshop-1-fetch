6897cf7461a9c5ea6cb44875ab9d5e749cefc1c0
leonelmarinaro <marinaro.leonel@tecnica7.edu.ar>
2023-03-02 11:47:24 -0300

    Modify index.html and index.js to fetch api avo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const urlBase = "https://platzi-avo.vercel.app/";
 
-console.log('Happy hacking :)')
+/* podemos obtener el fetch con Promise:
+windows
+    .fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then(data => {
+        console.log(data);
+    });
+Pero lo hacemos con asincronismo
+*/
+
+async function fetchData() {
+//   const response = await fetch(`${urlBase}api/avo`);
+  const response = await fetch(`https://platzi-avo.vercel.app/api/avo`);
+
+  const responseJson = await response.json();
+  const allItems = [];
+  //   return console.log(data);
+  responseJson.data.foreach((item) => {
+    // console.log(item.name);
+    const title = document.createElement("h2");
+
+    const container = document.createElement("div");
+    container.append(title);
+    allItems.push(container);
+  });
+  document.body.append(...allItems);
+}
+
+fetchData();
+// console.log(fetchData(`${urlBase}api/avo`));
